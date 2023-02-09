d08152758d9eac1ea9ceec7d5477bf459b16d46a
Mohamed Moulaye <109633435+MohamedM22@users.noreply.github.com>
2023-02-09 14:24:35 -0500

    Add files via upload

---
+++ IfElse.java
@@ -0,0 +1,20 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int grade = 65;
+
+        if (grade >= 80) {
+            System.out.println("You got an A");
+        } else if (grade >=70) {
+
+         System.out.println("You got an B");
+        } else if (grade >=60) {
+         System.out.println("You got an C");
+        } else if (grade >=50) {
+         System.out.println("You got an D");
+        }else {
+         System.out.println("Study hard next time!");
+        }
+            
+        
+    }
+}
